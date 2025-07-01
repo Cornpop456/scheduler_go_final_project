64e7a5885c1baaa589f2c052f2bace5f0b6e7887
Victor Ginzburg <v-viktor456@mail.ru>
2025-07-01 13:34:40 +0300

    add database

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,28 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/Cornpop456/scheduler_go_final_project/pkg/db"
 	"github.com/Cornpop456/scheduler_go_final_project/tests"
 )
 
 const WEB_DIR = "web"
 
 var PORT = ":" + strconv.Itoa(tests.Port)
+var DB_FILE = tests.DBFile
 
 func main() {
 	if os.Getenv("TODO_PORT") != "" {
 		PORT = ":" + os.Getenv("TODO_PORT")
+	}
+
+	if os.Getenv("TODO_DBFILE") != "" {
+		DB_FILE = os.Getenv("TODO_DBFILE")
+	}
+
+	err := db.Init(DB_FILE)
+
+	if err != nil {
+		log.Fatalf("Error initializing database: %v", err)
 	}
 
 	http.Handle("/", http.FileServer(http.Dir(WEB_DIR)))
---
+++ pkg/db/db.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "modernc.org/sqlite"
+)
+
+const schema = `
+CREATE TABLE scheduler  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date CHAR(8) NOT NULL DEFAULT "",
+    title VARCHAR(256) NOT NULL DEFAULT "",
+    comment TEXT NOT NULL DEFAULT "",
+    repeat VARCHAR(128) NOT NULL DEFAULT ""
+);
+CREATE INDEX scheduler_date_index ON scheduler (date);
+`
+
+var db *sql.DB
+
+func Init(dbFile string) error {
+	_, err := os.Stat(dbFile)
+
+	var install bool
+
+	if err != nil {
+		install = true
+	}
+
+	db, err = sql.Open("sqlite", dbFile)
+
+	if err != nil {
+		return fmt.Errorf("can't open database: %w", err)
+	}
+
+	if install {
+		_, err = db.Exec(schema)
+
+		if err != nil {
+			return fmt.Errorf("can't create schema: %w", err)
+		}
+	}
+
+	return nil
+}
